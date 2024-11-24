3dfd5e921d7e97c466240577d1701e21004df405
ArathSaenz <aratheduardosaenzcastelo@gmail.com>
2024-11-23 22:20:30 -0600

    En vez ed decir bienvenidos, ahora ya salen las mesas

--- ProyectoDB/ProyectoDB/Form1.vb
+++ ProyectoDB/ProyectoDB/Form1.vb
@@ -1,4 +1,7 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
+Public Class Form1
+
+    Dim conexion As New SqlConnection
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
@@ -19,5 +22,9 @@
 
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
 
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conexion = New SqlConnection("server=ARATH; database=Restaurante; integrated security=true")
     End Sub
 End Class
--- ProyectoDB/ProyectoDB/Form2.vb
+++ ProyectoDB/ProyectoDB/Form2.vb
@@ -1,3 +1,40 @@
 ﻿Public Class Form2
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
 
+    End Sub
+
+    Private Sub btnMesa6_Click(sender As Object, e As EventArgs) Handles btnMesa6.Click
+
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnMesa1_Click(sender As Object, e As EventArgs) Handles btnMesa1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        If (BackColor == Color.Lime) Then
+                {
+        FormBorderStyle = FormBorderStyle.Sizable;
+        BackColor = Color.Black;
+        Location = New Point(Left - 8, Top - 30);
+    }
+    Else
+    {
+        FormBorderStyle = FormBorderStyle.None;
+        BackColor = Color.Lime;
+        Location = New Point(Left + 8, Top + 30);
+    }
+    End Sub
 End Class
